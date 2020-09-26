fbc79e50e8b3271ced458bf23c8c0da3b6c88391
amirbaharvand66 <amirbaharvand66@yahoo.com>
2020-09-26 15:40:45 +0330

    fixed for single and double lamina(e)

--- moduli/lam_type.m
+++ moduli/lam_type.m
@@ -6,28 +6,46 @@ function [lam_typ] = lam_type(t, theta)
 % theta : ply angle
 % t : ply thickness
 
+% OUTPUT(S)
+% lam_type : laminate type
+
 % originally coded by Amir Baharvand (AB) (08-20)
+% fixed for single and double lamina(e) by AB (09-20)
 
+n_ply = length(t); % number of plies
 cnt = 0;
-lam_stck = zeros(1, ceil(length(theta) / 2));
+lam_stck = zeros(1, ceil(length(theta) / 2)); % laminate stacking sequence
 
-while cnt <= floor(length(theta) / 2)
-    cnt = cnt + 1;
-    lam_stck(cnt) = ( theta(cnt + 1) == theta( end - cnt) );
-end
-
-if isequal(t / max(t), ones(1, length(t))) == 1
-    
-    if isequal( lam_stck, ones(1, length(lam_stck)) ) && length(theta(theta < 0)) == length(theta(theta > 0))
-        lam_typ = 'symmetric-balanced';
-    elseif isequal( lam_stck, ones(1, length(lam_stck)) )
+switch n_ply
+    case 1 % single lamina
         lam_typ = 'symmetric';
-    elseif length(theta(theta < 0)) == length(theta(theta > 0))
-        lam_typ = 'balanced';
-    else
-        lam_typ = 'asymmetric';
-    end
-    
-else
-    lam_typ = 'asymmetric';
+    case 2 % double laminae
+        if theta(1) == theta(end)
+            lam_typ = 'unidirectional';
+        elseif theta(1) == -theta(end)
+            lam_typ = 'symmetric-balanced';
+        else
+            lam_typ = 'asymmetric';
+        end
+    otherwise % more than two laminae
+        
+        while cnt <= floor(length(theta) / 2)
+            lam_stck(cnt + 1) = ( theta(cnt + 1) == theta( end - cnt) );
+            cnt = cnt + 1;
+        end
+        if isequal(t / max(t), ones(1, length(t))) == 1 % if all plies are from the same thickness
+            
+            if isequal( lam_stck, ones(1, length(lam_stck)) ) && length(theta(theta < 0)) == length(theta(theta > 0))
+                lam_typ = 'symmetric-balanced';
+            elseif isequal( lam_stck, ones(1, length(lam_stck)) )
+                lam_typ = 'symmetric';
+            elseif length(theta(theta < 0)) == length(theta(theta > 0))
+                lam_typ = 'balanced';
+            else
+                lam_typ = 'asymmetric';
+            end
+            
+        else % if all plies are NOT from the same thickness
+            lam_typ = 'asymmetric';
+        end
 end
