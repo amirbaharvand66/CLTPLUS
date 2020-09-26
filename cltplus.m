bdfb6ae1bf53b05c249c74a667b9a842b48d903b
amirbaharvand66 <amirbaharvand66@yahoo.com>
2020-09-26 16:09:45 +0330

    organized cltplus.m

--- cltplus.m
+++ cltplus.m
@@ -2,53 +2,17 @@ clc
 clear
 close all
 
+addpath(genpath('./preamble'));
+initials()
 
-% Latex for plots
-set(groot, 'defaultAxesTickLabelInterpreter','latex'); set(groot, 'defaultLegendInterpreter','latex');
-
-
-% add paths of functions
-addpath(genpath('./abd'));
-addpath(genpath('./failure_criteria'));
-addpath(genpath('./mat_prop'));
-addpath(genpath('./moduli'));
-addpath(genpath('./plot_functions'));
-addpath(genpath('./report'));
-addpath(genpath('./design_measures'));
-addpath(genpath('./modules'));
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % material properties
+mat2  
 
-mat2 % for example
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% composite laminate theory module
-
-% 'load' = ‘nm’ force-moment
-%             ‘ek’ strain-survature
-
-% 'global' (plot global stress-strain) 'on' / 'off'
-
-% 'local' (plot local stress-strain) 'on' / 'off'
-
-% 'failure' = '' deafult value : no failure criterion
-% N.B. for the failure criteria, s11 and mainly represent fiber and matrix
-% failure, respectively.
-%                  'mstrs' maximum stress failure criterion
-%                  'mstrn' maximum strain failure criterion
-%                  'TH' Tsai-Hill stress failure criterion
-%                  'PHR' Puck (Hashin-Rotem) failure criterion
-%                  'all' compare all failure criteria
-
-% report 'on' / 'off'
-
+% composite laminate theory
+% type in "help cltplus_doc" in command window for documentation on input
+% parameters
 [laminate.(string(lamina_name))] = ...
     clt(mat, 'load', 'nm', 'global', 'off', 'local', 'off', 'failure', 'TH', 'report', 'off');
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
 
