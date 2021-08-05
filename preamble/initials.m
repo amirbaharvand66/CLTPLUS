63174b1f17a9976b7e282bc0f28283962b27bb05
amirbaharvand66 <amirbaharvand66@yahoo.com>
2021-08-05 18:56:02 +0430

    path to folders created

---
+++ preamble/initials.m
@@ -0,0 +1,8 @@
+function initials()
+% Latex for plots
+set(groot, 'defaultAxesTickLabelInterpreter','latex'); set(groot, 'defaultLegendInterpreter','latex');
+
+
+% add paths of functions
+addpath(genpath('./modules'));
+addpath(genpath('./lekhnitskii'));
