4c3ef28a21a77258e3a5dd15f194f9036acccfb7
amirbaharvand66 <amirbaharvand66@yahoo.com>
2021-08-05 20:23:44 +0430

    transformation matrix from cartesian to polar coordinate system added

---
+++ abd/transformation_matrix_plane_stress.m
@@ -0,0 +1,16 @@
+function [M] = transformation_matrix_plane_stress(theta)
+% transformation matrxi (M) for transforming each ply local coordinate
+% system to the global coordinate system
+
+% INPUT(S)
+% theta : ply angle
+
+% coded by Amir Baharvand (08-20)
+    
+for ii = 1:length(theta)
+    c = cosd(theta(ii));
+    s = sind(theta(ii));
+    M = [c^2            s^2             2 * s * c;
+             s^2            c^2             -2 * s * c;
+             -s * c          s * c            c^2 - s^2];
+end
