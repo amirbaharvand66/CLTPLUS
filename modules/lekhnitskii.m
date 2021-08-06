758d03314171417618d5f8ee1322cfe6f20bac01
amirbaharvand66 <amirbaharvand66@yahoo.com>
2021-08-06 10:36:45 +0430

    stress on hole edhe, x-axis, y-axis added

--- modules/lekhnitskii.m
+++ modules/lekhnitskii.m
@@ -1,17 +1,12 @@
-clc
-clear 
-close all
-
-laminate_name = 'mat2';
-
-% load laminate data from CLTPLUS
-load clt_output.mat
-
 % read data from clt.m
 h = laminate.(string(lamina_name)).ply.h; % h: laminate thickness
 A = laminate.(string(lamina_name)).abd.A; % A: A part of the ABD matrix
 p = mat.mat2.lekh.load; % load at infinity
 p_h = mat.mat2.lekh.hole; % load on hole edge
+R = 1; % hole radius
+epsilon = 1e-3;
+n = 100; % number of divisons
+theta = linspace(0 + epsilon, 360 - epsilon, n); % angle on the hole edge
 
 % laminate compliance matrix
 [S] = compliance_cal(h, A);
@@ -20,10 +15,33 @@ p_h = mat.mat2.lekh.hole; % load on hole edge
 [r, a] = dir_ang_cal(S);
 
 % characteristic equation roots
-[s, alpha, beta] = char_eqn_root(r, a);
+[s_k, alpha, beta] = char_eqn_root(r, a);
 
 % Homogeneous Stress Field Constant
-[h_k] = h_k_cal(p, s);
+[h_k] = h_k_cal(p, s_k);
 
-% Disturbance Field Constant
-[c_k] = c_k_cal(p, p_h, s);
+% Mapped Disturbance Field Constant
+[c_k] = c_k_cal(p, p_h, s_k);
+
+% Stress distribution on hole edge
+% stress_on_hole(epsilon, theta, R, alpha, beta, s_k, h_k, c_k)
+
+% Stress distribution on x-axis
+% stress_on_x(-10, 10, -8, 8, n, theta, alpha, beta, s_k, h_k, c_k)
+
+% Stress distribution on y-axis
+% stress_on_y(-8, 8, -10, 10, n, theta, alpha, beta, s_k, h_k, c_k)
+
+R = [1, 5, 10];
+mkr = {':', '--', '-'};
+% for ii = 1:length(R)
+%     sigma_r_on_hole(epsilon, theta, R(ii), alpha, beta, s_k, h_k, c_k, mkr{ii})
+% end
+
+% for ii = 1:length(R)
+%     sigma_theta_on_hole(epsilon, theta, R(ii), alpha, beta, s_k, h_k, c_k, mkr{ii})
+% end
+
+for ii = 1:length(R)
+    tau_rt_on_hole(epsilon, theta, R(ii), alpha, beta, s_k, h_k, c_k, mkr{ii})
+end
