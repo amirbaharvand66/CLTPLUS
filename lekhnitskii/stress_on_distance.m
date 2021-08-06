7764808d46c73bd6d94f8131125b3f86aa56bf04
amirbaharvand66 <amirbaharvand66@yahoo.com>
2021-08-06 12:00:40 +0430

    function comment added

--- lekhnitskii/stress_on_distance.m
+++ lekhnitskii/stress_on_distance.m
@@ -1,4 +1,15 @@
-function stress_on_distance(R)
+function stress_on_distance(epsilon, theta, alpha, beta, s_k, h_k, c_k, R)
+% Calculate stress components at various distance from hole edge
+
+% INPUT(S)
+% theta: angle on the hole edge
+% alpha: real part of s1 and s2
+% beta: imaginary part of s1 and s2
+% s_k: characteristic equation roots
+% h_k: homogeneous stress field constant
+% c_k: mapped disturbance field constant
+% R: a list of distance
+
 mkr = {':', '--', '-'};
 for ii = 1:length(R)
     sigma_r_on_hole(epsilon, theta, R(ii), alpha, beta, s_k, h_k, c_k, mkr{ii})
