8bb03489a0d53aafd2dc6a021fa2d772f5ce1110
amirbaharvand66 <amirbaharvand66@yahoo.com>
2021-08-06 10:33:28 +0430

    function comments edited

--- lekhnitskii/c_k_cal.m
+++ lekhnitskii/c_k_cal.m
@@ -1,13 +1,13 @@
-function [c_k] = c_k_cal(p, p_h, s)
-% Disturbance Field Constant
+function [c_k] = c_k_cal(p, p_h, s_k)
+% Mapped disturbance Field Constant
 
 % INPUT(S)
 % p: load at infinity
 % p_h: load on hole edge
-% s: characteristic equation roots
+% s_k: characteristic equation roots
 
 % OUTPUT(S)
-% c_k: disturbance field constant
+% c_k: mapped disturbance field constant
 
 syms c1 c2
 p_x = p(1);
@@ -17,8 +17,8 @@ Xc = p_h(1);
 Xs = p_h(2);
 Yc = p_h(3);
 Ys = p_h(4);
-s1 = s(1);
-s2 = s(2);
+s1 = s_k(1);
+s2 = s_k(2);
 
 [c1, c2]=solve( c1 + c2 == 1/2 * (1i * Yc - Ys - p_y + 1i * p_xy), ...
                         s1 * c1 + s2 * c2 == 1/2 * (Xs - 1i * Xc - 1i * p_x + p_xy) );
