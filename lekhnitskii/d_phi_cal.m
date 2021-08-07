c266f95733566e3ce6e551abdb778edf969d1865
amirbaharvand66 <amirbaharvand66@yahoo.com>
2021-08-07 19:10:46 +0430

    d_zeta corrected

--- lekhnitskii/d_phi_cal.m
+++ lekhnitskii/d_phi_cal.m
@@ -16,6 +16,6 @@ d_zeta = zeros(2, 1);
 d_phi = zeros(2, 1);
 
 for k = 1:2
-    d_zeta(k) = (1 / (1 + 1i *s_k(k))) * (1 - (z_k(k) / (sign_delta(k) * delta(k))));
+    d_zeta(k) = -((1 - 1i * s_k(k)) * (1 + z_k(k) / (sign_delta(k) * delta(k))) / (z_k(k) + sign_delta(k) * delta(k))^2);
     d_phi(k) = h_k(k) + c_k(k) * d_zeta(k);
 end
