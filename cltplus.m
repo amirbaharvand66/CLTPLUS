17624818b980fce3dac1b9e3877f10d6d6d76470
amirbaharvand66 <amirbaharvand66@yahoo.com>
2021-08-09 00:23:29 +0430

    lekhnitskii module added to cltplus.m file

--- cltplus.m
+++ cltplus.m
@@ -14,6 +14,12 @@ mat2
 [laminate.(string(lamina_name))] = ...
     clt(mat, 'load', 'nm', 'global', 'off', 'local', 'off', 'failure', 'TH', 'report', 'off');
 
+% type in "help lekhnitskii_doc" in command window for documentation, assumptions
+% and application of the present code
 
+laminate_name = 'mat2'; % laminate name as appeared in CLTPLUS
+clt_output = 'clt_output'; % data from CLTPLUS
+
+lekhnitskii(laminate_name, clt_output)
 
 
