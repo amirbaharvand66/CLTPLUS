db957cdd9cfb3e10a8c9a42f00063f3922b1fedb
amirbaharvand66 <amirbaharvand66@yahoo.com>
2021-08-10 02:04:34 +0430

    scaling deformed total displacement function created

---
+++ lekhnitskii/displ_normalizer.m
@@ -0,0 +1,18 @@
+function [split_tmp_con, d] = displ_normalizer(displ)
+% extract the scientific part of maximum total displacement (sqrt(c^2 + v^2))
+
+% INPUT(S)
+% displ: displacement around hole 
+
+% OUTPUT(S)
+% split_tmp_con: scientific part of maximum total displacement
+% d: resultant displacement
+
+u = displ(:, 1);
+v = displ(:, 2);
+
+d = sqrt(u.^2 + v.^2);
+tmp = max(d);
+str_tmp = string(tmp);
+split_tmp = strsplit(str_tmp, 'e');
+split_tmp_con = str2double(split_tmp{2});
