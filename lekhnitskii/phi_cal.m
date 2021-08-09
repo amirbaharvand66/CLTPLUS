12f9ae2779dfa6f1684fb8ab3d35a4db6baa1166
amirbaharvand66 <amirbaharvand66@yahoo.com>
2021-08-09 22:13:04 +0430

    phi calculator added

--- lekhnitskii/phi_cal.m
+++ lekhnitskii/phi_cal.m
@@ -1,9 +1,21 @@
-function [d_phi] = d_phi_cal(h_k, s_k, z_k, c_k, delta, sign_delta)
-% Phi_prime calculator
-d_zeta = zeros(2, 1);
-d_phi = zeroa(2, 1);
+function [phi] = phi_cal(h_k, s_k, z_k, c_k, delta, sign_delta)
+% Phi calculator
+
+% INPUT(S)
+% h_k: homogeneous stress field constant
+% s_k: characteristic equation roots
+% z_k = x + s_k * y
+% c_k: mapped disturbance field constant
+% delta = sqrt(z_k^2 - s_k^2 - 1)
+% sign_delta: sign of delta
+
+% OUTPUT(S)
+% phi: Phi
+
+zeta = zeros(2, 1);
+phi = zeros(2, 1);
 
 for k = 1:2
-    d_zeta(k) = (1 / (1 + 1i *s_k(k))) * (1 - (z_k(k) / (sign_delta(k) * delta(k))));
-    d_phi(k) = h_k(k) + c_k(k) * d_zeta(k);
+    zeta(k) = (z_k(k) + sign_delta(k) * delta(k)) / (1 - 1i * s_k(k));
+    phi(k) = h_k(k) * z_k(k) + c_k(k) * zeta(k)^(-1);
 end
