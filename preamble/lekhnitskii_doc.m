d5078c46938304b5d48bd10c24f6620f827b122e
amirbaharvand66 <amirbaharvand66@yahoo.com>
2021-08-10 11:53:53 +0430

    lekhnistkii documentation added

---
+++ preamble/lekhnitskii_doc.m
@@ -0,0 +1,19 @@
+function lekhnitskii_doc()
+%                           Lekhnitskii Solution
+% Lekhnitskii formulation for stress concentration 
+% around a circular hole in a thin orthotropic plate
+
+
+% -------------------Assumptions-------------------
+% 1.Size of the cut-off is negligible compared to the 
+% domain geometrical dimension
+% 2. No body force
+% 3. Loads on the edge are harmonic
+% 4. Simply/multiply-connected domains
+
+% --------------------Limitations--------------------
+% 1. Support an orthotrpic plate with a circular hole
+% 2. The traction boundary-value problem
+% 3. Thin plate where Composite Classical Laminate 
+% Theory is applicable
+% 4. A circular hole with unit radius
