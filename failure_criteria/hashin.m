fe3ea84982afbed96b08ae9bb85b15b5252f5b8e
Amir Baharvand <amirbaharvand66@yahoo.com>
2022-12-06 16:08:13 -0500

    factor of safety was added

--- failure_criteria/hashin.m
+++ failure_criteria/hashin.m
@@ -25,6 +25,7 @@ ply_num = zeros(length(theta) * 2, 1);
 flag_vec = zeros(length(theta) * 2, 1);
 theta_c = theta(ceil((1:length(theta) * 2) / 2))';
 ft_vec = cell(length(theta) * 2, 1);
+sf_vec = zeros(length(theta) * 2, 1);
 position = repmat({'top surface'; 'bottom surface'}, length(theta), 1);
 
 for ii = 1:size(ls, 1)
@@ -37,6 +38,7 @@ for ii = 1:size(ls, 1)
 
     ffail = (s11 / s__x)^2; % fiber failure
     mfail = (s22 / s__y)^2 + (s12/ S)^2; % matrix failure
+    sf = min(1 / sqrt(ffail), 1 / sqrt(mfail)); % safety factor
 
     if ffail > mfail
         % fiber failure at top surface
@@ -133,6 +135,9 @@ for ii = 1:size(ls, 1)
 
     % collecting failure type
     ft_vec{ii} = ft;
+
+    % collecting safety factor
+    sf_vec(ii) = sf;
 end
 
 % in case of failure of either top or bottom surface the whole ply should
@@ -150,8 +155,9 @@ ply_num = num2cell(ply_num);
 flag_vec = num2cell(flag_vec);
 theta_c = num2cell(theta_c);
 fidx_vec = repmat({'NA'}, length(theta) * 2, 1);
-fail_tag = {'Ply no.', 'Ply angle', 'Position', 'Status', 'Failure index', 'Failure type'};
-fail_rpt = [fail_tag; ply_num, theta_c, position, flag_vec, fidx_vec, ft_vec]; % failure report
+sf_vec = num2cell(sf_vec);
+fail_tag = {'Ply no.', 'Ply angle', 'Position', 'Status', 'Failure index', 'Safety factor', 'Failure type'};
+fail_rpt = [fail_tag; ply_num, theta_c, position, flag_vec, fidx_vec, sf_vec, ft_vec]; % failure report
 
 
 % displaying failure report
@@ -159,13 +165,13 @@ fprintf('**********************************************************************\
 fprintf('\tPuck (Hashin-Rotem) Failure Criterion - Laminate %d\n', id)
 fprintf('\t    FAILURE = 1     Without failure = 0\n')
 fprintf('**********************************************************************\n')
-fprintf('Ply no. \t Ply angle \t Position \t\t Status \t Failure index \t Failure type\n')
+fprintf('Ply no. \t Ply angle \t Position \t\t Status \t Failure index \t Safety factor \t Failure type\n')
 
 for ii = 2:size(fail_rpt, 1)
     if mod(ii, 2) == 0
-        fprintf('%0.0f \t %d \t\t %s \t\t %d \t %s \t\t %s\n', fail_rpt{ii, :})
+        fprintf('%0.0f \t %d \t\t %s \t\t %d \t %s \t\t %3.2f \t\t %s\n', fail_rpt{ii, :})
     else
-        fprintf('%0.0f \t %d \t\t %s \t %d \t %s \t\t %s\n', fail_rpt{ii, :})
+        fprintf('%0.0f \t %d \t\t %s \t %d \t %s \t\t %3.2f \t\t %s\n', fail_rpt{ii, :})
     end
 end
 
